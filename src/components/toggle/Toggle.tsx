099d78e424cad463b2c01c64e0b75c559cc4d8d6
Taulant-kutia <44265863+taulantisaku@users.noreply.github.com>
2022-06-24 17:07:53 +0200

    Add audio click to button switch theme

--- src/components/toggle/Toggle.tsx
+++ src/components/toggle/Toggle.tsx
@@ -3,12 +3,27 @@ import Sun from "../../img/sun.png";
 import Moon from "../../img/moon.png";
 import { useContext } from "react";
 import { ThemeContext } from "../../lib/context/ThemeContext";
+import SwitchOff from "../../audio/lightswitch-off.mp3";
+import SwitchOn from "../../audio/lightswitch-on.mp3";
+
+//lib
+import { Howl } from "howler";
 
 export const Toggle = () => {
   const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
 
   const handleClick = () => {
     theme.dispatch({ type: "TOGGLE" });
+    darkMode ? SoundPlay(SwitchOff) : SoundPlay(SwitchOn);
+  };
+
+  const SoundPlay = (src: any) => {
+    const sound = new Howl({
+      src,
+      html5: true,
+    });
+    sound.play();
   };
 
   return (
